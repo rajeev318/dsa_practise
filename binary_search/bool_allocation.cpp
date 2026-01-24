b375af7b584e87738022f355dfcabd3607ed6987
rajeev318 <rajeevbaghel243@gmail.com>
2026-01-24 17:27:33 +0530

    updated book allocation solution

---
+++ binary_search/bool_allocation.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+int book_allocation(int arr[],int k, int n)
+{
+    int sum = 0, maxi = arr[0];
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+        maxi = max(maxi, arr[i]);
+    }
+    int start = maxi, end = sum, ans = -1;
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+        int page_sum = 0, student_count = 1;
+        for (int i = 0; i < n; i++)
+        {
+            page_sum += arr[i];
+            if (page_sum > mid)
+            {
+                student_count++;
+                page_sum = arr[i];
+            }
+        }
+        if (student_count <= k){
+            ans=mid;
+            end=mid-1;
+
+        }
+        else
+        start=mid+1;
+    }
+    return ans;
+}
+int main()
+{
+    int arr[]={12,34,67,90};
+    int k=2;
+    cout<<book_allocation(arr,k,4)<<endl;
+
+
+}
